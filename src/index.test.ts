37b5e7bb85d1a0bf889425d6aa0a9128273615d4
GersonUmanzor <gbumanzor@outlook.com>
2020-11-06 16:14:23 -0600

    :white_check_mark: Create tests for the library.

---
+++ src/index.test.ts
@@ -0,0 +1,31 @@
+import { convertPayloadKeys, validateObject } from './index';
+
+test('validateObject function', () => {
+  const testFn = () => 'Hello world';
+  const testArray = ['John', 'Doe'];
+  const testObject = { name: 'John', lastName: 'Doe' };
+
+  expect(validateObject(testFn)).toBe(false);
+  expect(validateObject(testArray)).toBe(false);
+  expect(validateObject(testObject)).toBeTruthy();
+});
+
+test('convertPayloadFunction', () => {
+  const previousPayload = { first_name: 'John', last_name: 'Doe' };
+  const transformedPayload = convertPayloadKeys(previousPayload, 'camelCase');
+
+  expect(transformedPayload).toEqual({ firstName: 'John', lastName: 'Doe' });
+
+  expect(
+    convertPayloadKeys({ another_snake_case: 'Key' }, 'camelCase')
+  ).toEqual({ anotherSnakeCase: 'Key' });
+
+  expect(convertPayloadKeys(previousPayload, 'snakeCase')).toEqual(
+    previousPayload
+  );
+
+  expect(convertPayloadKeys(previousPayload, 'kebabCase')).toEqual({
+    'first-name': 'John',
+    'last-name': 'Doe',
+  });
+});
