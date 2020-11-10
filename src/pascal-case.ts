c02fcb3cbab13c64c996ab80c54830f703fe6ae1
GersonUmanzor <gbumanzor@outlook.com>
2020-11-10 13:33:10 -0600

    :sparkles: Add PascalCase.

---
+++ src/pascal-case.ts
@@ -0,0 +1,9 @@
+import { camelCase } from 'lodash';
+import { memoize } from './memoize';
+
+const _camelCase = memoize(camelCase);
+
+export const pascalCase = (key) => {
+  const camelCased = _camelCase(key) as string;
+  return `${camelCased.slice(0, 1).toUpperCase()}${camelCased.slice(1)}`;
+};
