43abb3ba7d6dfd49b5c48fae96da09d492e36402
GersonUmanzor <gbumanzor@outlook.com>
2020-11-13 17:07:50 -0600

    :fire: Remove file.

--- src/conversion-cases.ts
+++
@@ -1,7 +0,0 @@
-import { toCamelCase, toKebabCase, toPascalCase, toSnakeCase } from './cases';
-import { memoize } from './utils/memoize';
-
-export const camelCase = memoize(toCamelCase);
-export const snakeCase = memoize(toSnakeCase);
-export const kebabCase = memoize(toKebabCase);
-export const pascalCase = memoize(toPascalCase);
